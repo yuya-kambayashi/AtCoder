51ba59e107e4500617ea2c542501b4a19f97c994
yuya-kambayashi <kambayashi73@gmail.com>
2025-02-07 09:40:09 +0900

    下から

--- AtCoder/src/main/java/org/example/ABC252D.java
+++ AtCoder/src/main/java/org/example/ABC252D.java
@@ -34,12 +34,32 @@ public class ABC252D {
 //import java.util.stream.*;
 //public class Main {
 
+    // https://atcoder.jp/contests/abc252/submissions/31907409
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         final int n = sc.nextInt();
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            int a = sc.nextInt();
+            map.put(a, map.getOrDefault(a, 0) + 1);
+        }
+        long total = (long) n * (n - 1) * (n - 2) / 6;
 
-        System.out.println();
+        for (int i : map.keySet()) {
+            long num = (long) map.get(i);
+            long dec = 0;
+            if (num >= 3) {
+                dec += num * (num - 1) * (num - 2) / 6;
+            }
+            if (num >= 2) {
+                dec += (num * (num - 1) / 2) * (n - num);
+            }
+            total -= dec;
+        }
+
+        System.out.println(total);
     }
 //}
 
@@ -60,7 +80,7 @@ public class ABC252D {
         Stream.of(expected.split("\\n")).map(String::trim).forEach(s -> assertThat(out.readLine().trim()).isEqualTo(s));
     }
 
-    //  @Test
+    @Test
     public void Case2() {
 
         String input = """
@@ -77,7 +97,7 @@ public class ABC252D {
         Stream.of(expected.split("\\n")).map(String::trim).forEach(s -> assertThat(out.readLine().trim()).isEqualTo(s));
     }
 
-    // @Test
+    @Test
     public void Case3() {
 
         String input = """
@@ -94,7 +114,7 @@ public class ABC252D {
         Stream.of(expected.split("\\n")).map(String::trim).forEach(s -> assertThat(out.readLine().trim()).isEqualTo(s));
     }
 
-    // @Test
+    //@Test
     public void Case4() {
 
         String input = """
