f8a81d975554c9fa66c223d3f50eda86a568bfaf
yuya-kambayashi <kambayashi73@gmail.com>
2025-02-13 22:25:40 +0900

    下から

--- AtCoder/src/main/java/org/example/ABC268D.java
+++ AtCoder/src/main/java/org/example/ABC268D.java
@@ -34,12 +34,76 @@ public class ABC268D {
 //import java.util.stream.*;
 //public class Main {
 
+    // https://atcoder.jp/contests/abc268/submissions/39730428
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         final int n = sc.nextInt();
+        final int m = sc.nextInt();
+        String[] ss = new String[n];
+        int len = 0;
+        for (int i = 0; i < n; i++) {
+            ss[i] = sc.next();
+            len += ss[i].length();
+        }
+        String[] tt = new String[m];
+        for (int i = 0; i < m; i++) {
+            tt[i] = sc.next();
+        }
+        Arrays.sort(ss);
+        Arrays.sort(tt);
 
-        System.out.println();
+        boolean first = true;
+        while (first || next_permutation(ss)) {
+            first = false;
+            generate(0, 16 - len + 1 - n, "", ss, tt);
+        }
+
+        System.out.println(-1);
+    }
+
+    static boolean next_permutation(String[] pp) {
+        for (int a = pp.length - 2; a >= 0; a--) {
+            if (pp[a].compareTo(pp[a + 1]) < 0) {
+                for (int b = pp.length - 1; ; b--) {
+                    if (pp[b].compareTo(pp[a]) > 0) {
+                        String t = pp[a];
+                        pp[a] = pp[b];
+                        pp[b] = t;
+                        for (++a, b = pp.length - 1; a < b; ++a, --b) {
+                            t = pp[a];
+                            pp[a] = pp[b];
+                            pp[b] = t;
+                        }
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
+    static void generate(int cur, int len, String res, String[] ss, String[] tt) {
+        if (len < 0) {
+            return;
+        }
+        if (cur == ss.length) {
+            if (res.length() >= 3 && Arrays.binarySearch(tt, res) < 0) {
+                System.out.println(res);
+                System.exit(0);
+            }
+            return;
+        }
+
+        if (res.length() > 0 && res.charAt(res.length() - 1) != '_') {
+            generate(cur, len, res.concat("_"), ss, tt);
+        } else {
+            generate(cur + 1, len, res.concat(ss[cur]), ss, tt);
+            if (res.length() > 0) {
+                generate(cur, len - 1, res.concat("_"), ss, tt);
+            }
+        }
     }
 //}
 
@@ -61,7 +125,7 @@ public class ABC268D {
         Stream.of(expected.split("\\n")).map(String::trim).forEach(s -> assertThat(out.readLine().trim()).isEqualTo(s));
     }
 
-    //  @Test
+    @Test
     public void Case2() {
 
         String input = """
@@ -81,7 +145,7 @@ public class ABC268D {
         Stream.of(expected.split("\\n")).map(String::trim).forEach(s -> assertThat(out.readLine().trim()).isEqualTo(s));
     }
 
-    // @Test
+    @Test
     public void Case3() {
 
         String input = """
@@ -101,7 +165,7 @@ public class ABC268D {
         Stream.of(expected.split("\\n")).map(String::trim).forEach(s -> assertThat(out.readLine().trim()).isEqualTo(s));
     }
 
-    // @Test
+    @Test
     public void Case4() {
 
         String input = """
