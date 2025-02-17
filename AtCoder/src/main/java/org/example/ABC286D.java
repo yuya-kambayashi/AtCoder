66b4d46012b1bd3d39f0bb9dd40b434259997b2e
yuya-kambayashi <kambayashi73@gmail.com>
2025-02-17 10:37:23 +0900

    ABC393

--- AtCoder/src/main/java/org/example/ABC286D.java
+++ AtCoder/src/main/java/org/example/ABC286D.java
@@ -34,12 +34,39 @@ public class ABC286D {
 //import java.util.stream.*;
 //public class Main {
 
+    // https://atcoder.jp/contests/abc286/submissions/44428338
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         final int n = sc.nextInt();
+        final int x = sc.nextInt();
+        int[] aa = new int[n];
+        int[] bb = new int[n];
+        for (int i = 0; i < n; i++) {
+            aa[i] = sc.nextInt();
+            bb[i] = sc.nextInt();
+        }
+        boolean[] cc = new boolean[x + 1];
+        cc[0] = true;
 
-        System.out.println();
+        for (int p = 0; p < n; p++) {
+            int a = aa[p];
+            int b = bb[p];
+            boolean[] next = new boolean[x + 1];
+            for (int i = 0; i <= x; i++) {
+                for (int j = 0; j <= b && i + a * j <= x; j++) {
+
+                    if (cc[i]) {
+                        next[i + a * j] = true;
+                    }
+                }
+            }
+            cc = next;
+        }
+
+
+        System.out.println(cc[x] ? "Yes" : "No");
     }
 //}
 
@@ -61,7 +88,7 @@ public class ABC286D {
         Stream.of(expected.split("\\n")).map(String::trim).forEach(s -> assertThat(out.readLine().trim()).isEqualTo(s));
     }
 
-    //  @Test
+    @Test
     public void Case2() {
 
         String input = """
@@ -79,7 +106,7 @@ public class ABC286D {
         Stream.of(expected.split("\\n")).map(String::trim).forEach(s -> assertThat(out.readLine().trim()).isEqualTo(s));
     }
 
-    // @Test
+    @Test
     public void Case3() {
 
         String input = """
