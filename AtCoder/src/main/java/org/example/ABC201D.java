4a3b003f2f9f4a17b1df5fc6dc8156734faf0f33
yuya-kambayashi <kambayashi73@gmail.com>
2025-02-25 10:51:42 +0900

    下から

--- AtCoder/src/main/java/org/example/ABC201D.java
+++ AtCoder/src/main/java/org/example/ABC201D.java
@@ -24,75 +24,76 @@ public class ABC201D {
     }
 
     @AfterAll
-    public void afterAll() {//import java.math.*;
+    public void afterAll() {
+        System.setIn(null);
+        System.setOut(null);
+    }
+    //import java.math.*;
 //import java.util.*;
 //import java.util.stream.*;
 //public class Main {
 
-        // https://atcoder.jp/contests/abc201/submissions/44842311
-        // #DP
+    // https://atcoder.jp/contests/abc201/submissions/44842311
+    // #DP
 
-        public static void main(String[] args) {
-            Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-            int h = sc.nextInt();
-            int w = sc.nextInt();
+        int h = sc.nextInt();
+        int w = sc.nextInt();
 
-            int[][] ggg = new int[h][w];
-            for (int i = 0; i < h; i++) {
-                String s = sc.next();
-                int j = 0;
-                for (char c : s.toCharArray()) {
-                    if (c == '+') {
-                        ggg[i][j] = 1;
-                    } else {
-                        ggg[i][j] = -1;
-                    }
-                    j++;
+        int[][] ggg = new int[h][w];
+        for (int i = 0; i < h; i++) {
+            String s = sc.next();
+            int j = 0;
+            for (char c : s.toCharArray()) {
+                if (c == '+') {
+                    ggg[i][j] = 1;
+                } else {
+                    ggg[i][j] = -1;
+                }
+                j++;
+            }
+        }
+        for (int i = 0; i < h; i++) {
+            for (int j = 0; j < w; j++) {
+                if ((i + j) % 2 == 0) {
+                    ggg[i][j] *= -1;
                 }
             }
-            for (int i = 0; i < h; i++) {
-                for (int j = 0; j < w; j++) {
-                    if ((i + j) % 2 == 0) {
-                        ggg[i][j] *= -1;
-                    }
-                }
-            }
-            int[][] scores = new int[h][w];
-            scores[h - 1][w - 1] = 0;
-            for (int y = h - 2; y >= 0; y--) {
-                scores[y][w - 1] = scores[y + 1][w - 1] + ggg[y + 1][w - 1];
-            }
+        }
+        int[][] scores = new int[h][w];
+        scores[h - 1][w - 1] = 0;
+        for (int y = h - 2; y >= 0; y--) {
+            scores[y][w - 1] = scores[y + 1][w - 1] + ggg[y + 1][w - 1];
+        }
+        for (int x = w - 2; x >= 0; x--) {
+            scores[h - 1][x] = scores[h - 1][x + 1] + ggg[h - 1][x + 1];
+        }
+        for (int y = h - 2; y >= 0; y--) {
             for (int x = w - 2; x >= 0; x--) {
-                scores[h - 1][x] = scores[h - 1][x + 1] + ggg[h - 1][x + 1];
-            }
-            for (int y = h - 2; y >= 0; y--) {
-                for (int x = w - 2; x >= 0; x--) {
-                    int down = scores[y + 1][x] + ggg[y + 1][x];
-                    int right = scores[y][x + 1] + ggg[y][x + 1];
-                    if (isMaxmize(x, y)) {
-                        scores[y][x] = Math.max(down, right);
-                    } else {
-                        scores[y][x] = Math.min(down, right);
-                    }
+                int down = scores[y + 1][x] + ggg[y + 1][x];
+                int right = scores[y][x + 1] + ggg[y][x + 1];
+                if (isMaxmize(x, y)) {
+                    scores[y][x] = Math.max(down, right);
+                } else {
+                    scores[y][x] = Math.min(down, right);
                 }
             }
-            String ret = "Draw";
-            if (scores[0][0] > 0) {
-                ret = "Takahashi";
-            } else if (scores[0][0] < 0) {
-                ret = "Aoki";
-            }
-            System.out.println(ret);
         }
-
-        static boolean isMaxmize(int x, int y) {
-            return (x + y) % 2 == 0;
+        String ret = "Draw";
+        if (scores[0][0] > 0) {
+            ret = "Takahashi";
+        } else if (scores[0][0] < 0) {
+            ret = "Aoki";
         }
-//}
-        System.setIn(null);
-        System.setOut(null);
+        System.out.println(ret);
     }
+
+    static boolean isMaxmize(int x, int y) {
+        return (x + y) % 2 == 0;
+    }
+//}
 
 
     @Test
