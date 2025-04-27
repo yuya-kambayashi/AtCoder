0a3d9b9b2b614111bd2991e9094bcfde4d19c7eb
Yuya KAMBAYASHI <kambayashi73@gmail.com>
2025-04-27 22:53:16 +0900

    ABC399

---
+++ AtCoder/src/main/java/org/example/ABC403B.java
@@ -0,0 +1,132 @@
+package org.example;
+
+import java.util.*;
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class ABC403B {
+
+    private final StandardInputSnatcher in = new StandardInputSnatcher();
+    private final StandardOutputSnatcher out = new StandardOutputSnatcher();
+
+    @BeforeAll
+    public void beforeAll() {
+        System.setIn(in);
+        System.setOut(out);
+    }
+
+    @AfterAll
+    public void afterAll() {
+        System.setIn(null);
+        System.setOut(null);
+    }
+//import java.math.*;
+//import java.util.*;
+//import java.util.stream.*;
+//public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        final String t = sc.next();
+        final String u = sc.next();
+
+
+        for (int i = 0; i < t.length() - u.length() + 1; i++) {
+            String st = t.substring(i, i + u.length());
+
+            int diff = 0;
+            for (int j = 0; j < st.length(); j++) {
+
+                if (st.charAt(j) == '?') {
+                    continue;
+                } else if (st.charAt(j) != u.charAt(j)) {
+                    diff++;
+                    break;
+                }
+            }
+            if (diff == 0) {
+                System.out.println("Yes");
+                return;
+            }
+        }
+
+
+        System.out.println("No");
+    }
+
+//}
+
+    @Test
+    public void Case1() {
+
+        String input = """
+                tak??a?h?
+                nashi
+                
+                """;
+
+        String expected = """
+                Yes
+                """;
+        Stream.of(input.split("\\n")).map(String::trim).forEach(in::inputln);
+        ABC403B.main(null);
+        Stream.of(expected.split("\\n")).map(String::trim).forEach(s -> assertThat(out.readLine().trim()).isEqualTo(s));
+    }
+
+    @Test
+    public void Case2() {
+
+        String input = """
+                ??e??e
+                snuke
+                
+                """;
+
+        String expected = """
+                No
+                """;
+        Stream.of(input.split("\\n")).map(String::trim).forEach(in::inputln);
+        ABC403B.main(null);
+        Stream.of(expected.split("\\n")).map(String::trim).forEach(s -> assertThat(out.readLine().trim()).isEqualTo(s));
+    }
+
+    @Test
+    public void Case3() {
+
+        String input = """
+                ????
+                aoki
+                
+                """;
+
+        String expected = """
+                Yes
+                """;
+        Stream.of(input.split("\\n")).map(String::trim).forEach(in::inputln);
+        ABC403B.main(null);
+        Stream.of(expected.split("\\n")).map(String::trim).forEach(s -> assertThat(out.readLine().trim()).isEqualTo(s));
+    }
+
+    //@Test
+    public void Case4() {
+
+        String input = """
+                
+                """;
+
+        String expected = """
+                
+                """;
+        Stream.of(input.split("\\n")).map(String::trim).forEach(in::inputln);
+        ABC403B.main(null);
+        Stream.of(expected.split("\\n")).map(String::trim).forEach(s -> assertThat(out.readLine().trim()).isEqualTo(s));
+    }
+}
