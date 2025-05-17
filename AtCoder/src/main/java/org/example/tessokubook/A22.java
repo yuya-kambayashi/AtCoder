305cb5d303cbee051ff374abfb79962bfa7cc7a4
yuya-kambayashi <kambayashi73@gmail.com>
2025-05-17 20:49:30 +0900

    tessokubook

--- AtCoder/src/main/java/org/example/tessokubook/A22.java
+++ AtCoder/src/main/java/org/example/tessokubook/A22.java
@@ -9,6 +9,7 @@ import org.example.template.StandardInputSnatcher;
 import org.example.template.StandardOutputSnatcher;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
@@ -36,35 +37,58 @@ public class A22 {
         Scanner sc = new Scanner(System.in);
 
         final int n = sc.nextInt();
+        int[] aa = new int[n];
+        int[] bb = new int[n];
+        for (int i = 1; i < n; i++) {
+            aa[i] = sc.nextInt();
+        }
+        for (int i = 1; i < n; i++) {
+            bb[i] = sc.nextInt();
+        }
+        int[] dp = new int[n + 1];
+        Arrays.fill(dp, Integer.MIN_VALUE);
+        dp[1] = 0;
 
-        System.out.println(n);
+        for (int i = 1; i <= n - 1; i++) {
+            dp[aa[i]] = Math.max(dp[aa[i]], dp[i] + 100);
+            dp[bb[i]] = Math.max(dp[bb[i]], dp[i] + 150);
+        }
+
+
+        System.out.println(dp[n]);
     }
 //}
 
-    //@Test
+    @Test
     public void Case1() {
 
         String input = """
+                7
+                2 4 4 7 6 7
+                3 5 6 7 7 7
                 
                 """;
 
         String expected = """
-                
+                500
                 """;
         Stream.of(input.split("\\n")).map(String::trim).forEach(in::inputln);
         A22.main(null);
         Stream.of(expected.split("\\n")).map(String::trim).forEach(s -> assertThat(out.readLine().trim()).isEqualTo(s));
     }
 
-    //@Test
+    @Test
     public void Case2() {
 
         String input = """
+                2
+                2
+                2
                 
                 """;
 
         String expected = """
-                
+                150
                 """;
         Stream.of(input.split("\\n")).map(String::trim).forEach(in::inputln);
         A22.main(null);
