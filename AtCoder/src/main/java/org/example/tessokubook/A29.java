6f26b250fde6ceb5dffd9e6da816257d42df3aa3
yuya-kambayashi <kambayashi73@gmail.com>
2025-05-20 20:03:08 +0900

    tessokubook

--- AtCoder/src/main/java/org/example/tessokubook/A29.java
+++ AtCoder/src/main/java/org/example/tessokubook/A29.java
@@ -9,6 +9,7 @@ import org.example.template.StandardInputSnatcher;
 import org.example.template.StandardOutputSnatcher;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
@@ -32,23 +33,42 @@ public class A29 {
 //import java.util.*;
 //public class Main {
 
+    // https://atcoder.jp/contests/tessoku-book/submissions/55570443
+
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        final int n = sc.nextInt();
+        long a = sc.nextLong();
+        int b = sc.nextInt();
 
-        System.out.println(n);
+        int mod = 1000000007;
+
+
+        long ans = 1;
+        while (b >= 1) {
+            if (b % 2 == 1) {
+                ans = (ans * a) % mod;
+            }
+            a = (a * a) % mod;
+            b = b >> 1;
+        }
+
+
+        System.out.println(ans);
     }
 //}
 
-    //@Test
+    @Test
     public void Case1() {
 
         String input = """
+                5 23
                 
                 """;
 
         String expected = """
+                871631629
                 
                 """;
         Stream.of(input.split("\\n")).map(String::trim).forEach(in::inputln);
@@ -56,14 +76,16 @@ public class A29 {
         Stream.of(expected.split("\\n")).map(String::trim).forEach(s -> assertThat(out.readLine().trim()).isEqualTo(s));
     }
 
-    //@Test
+    @Test
     public void Case2() {
 
         String input = """
+                97 998244353
                 
                 """;
 
         String expected = """
+                934801994
                 
                 """;
         Stream.of(input.split("\\n")).map(String::trim).forEach(in::inputln);
