3a37612de583fbfeb32c65432107724b80d315f1
yuya-kambayashi <kambayashi73@gmail.com>
2025-05-20 19:32:24 +0900

    tessokubook

--- AtCoder/src/main/java/org/example/tessokubook/A28.java
+++ AtCoder/src/main/java/org/example/tessokubook/A28.java
@@ -9,6 +9,7 @@ import org.example.template.StandardInputSnatcher;
 import org.example.template.StandardOutputSnatcher;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
@@ -32,23 +33,51 @@ public class A28 {
 //import java.util.*;
 //public class Main {
 
+    // https://atcoder.jp/contests/tessoku-book/submissions/56439806
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         final int n = sc.nextInt();
+        long p = 0;
+        for (int i = 0; i < n; i++) {
+            String s = sc.next();
+            char c = s.charAt(0);
+            int a = sc.nextInt();
 
-        System.out.println(n);
+            if (c == '+') {
+                p += a;
+
+            } else if (c == '-') {
+                p -= a;
+
+            } else if (c == '*') {
+                p *= a;
+            }
+            p %= 10000;
+            p = (p + 10000) % 10000;
+            System.out.println(p);
+        }
     }
 //}
 
-    //@Test
+    @Test
     public void Case1() {
 
         String input = """
+                4
+                + 57
+                + 43
+                * 100
+                - 1
                 
                 """;
 
         String expected = """
+                57
+                100
+                0
+                9999
                 
                 """;
         Stream.of(input.split("\\n")).map(String::trim).forEach(in::inputln);
