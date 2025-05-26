c58ee65f3a89c79bcafa858e190329afcc412b3e
yuya-kambayashi <kambayashi73@gmail.com>
2025-05-26 10:51:09 +0900

    tessokubook

--- AtCoder/src/main/java/org/example/tessokubook/A35.java
+++ AtCoder/src/main/java/org/example/tessokubook/A35.java
@@ -9,6 +9,7 @@ import org.example.template.StandardInputSnatcher;
 import org.example.template.StandardOutputSnatcher;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
@@ -32,24 +33,45 @@ public class A35 {
 //import java.util.*;
 //public class Main {
 
+    // https://atcoder.jp/contests/tessoku-book/submissions/35381725
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         final int n = sc.nextInt();
+        int[] aa = new int[n];
+        for (int i = 0; i < n; i++) {
+            aa[i] = sc.nextInt();
+        }
+        int[][] dp = new int[n + 1][n + 1];
+        for (int i = 1; i <= n; i++) {
+            dp[n][i] = aa[i - 1];
+        }
+        for (int i = n - 1; i >= 1; i--) {
+            for (int j = 1; j <= i; j++) {
+                if (i % 2 == 1) {
+                    dp[i][j] = Math.max(dp[i + 1][j], dp[i + 1][j + 1]);
+                } else {
+                    dp[i][j] = Math.min(dp[i + 1][j], dp[i + 1][j + 1]);
+                }
+            }
+        }
 
-        System.out.println(n);
+        System.out.println(dp[1][1]);
     }
 //}
 
-    //@Test
+    @Test
     public void Case1() {
 
         String input = """
+                4
+                20 10 30 40
                 
                 """;
 
         String expected = """
-                
+                30
                 """;
         Stream.of(input.split("\\n")).map(String::trim).forEach(in::inputln);
         A35.main(null);
