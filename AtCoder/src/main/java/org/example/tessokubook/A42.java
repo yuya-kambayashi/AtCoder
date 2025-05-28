84fb36607686d7694c433475f3f024bbaad8a77b
yuya-kambayashi <kambayashi73@gmail.com>
2025-05-28 11:27:44 +0900

    tessokubook

--- AtCoder/src/main/java/org/example/tessokubook/A42.java
+++ AtCoder/src/main/java/org/example/tessokubook/A42.java
@@ -9,6 +9,7 @@ import org.example.template.StandardInputSnatcher;
 import org.example.template.StandardOutputSnatcher;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
@@ -36,20 +37,50 @@ public class A42 {
         Scanner sc = new Scanner(System.in);
 
         final int n = sc.nextInt();
+        final int k = sc.nextInt();
+        int[] aa = new int[n + 1];
+        int[] bb = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            aa[i] = sc.nextInt();
+            bb[i] = sc.nextInt();
+        }
+        int max = 0;
+        for (int i = 1; i <= 100; i++) {
+            for (int j = 1; j <= 100; j++) {
+                int count = getScore(i, j, k, aa, bb);
+                if (max < count) {
+                    max = count;
+                }
+            }
+        }
+        System.out.println(max);
+    }
 
-        System.out.println(n);
+    static int getScore(int minA, int minB, int k, int[] aa, int[] bb) {
+        int count = 0;
+        for (int i = 1; i < aa.length; i++) {
+            if (minA <= aa[i] && minB <= bb[i] && minA + k >= aa[i] && minB + k >= bb[i]) {
+                count++;
+            }
+        }
+        return count;
     }
 //}
 
-    //@Test
+    @Test
     public void Case1() {
 
         String input = """
+                4 30
+                20 30
+                10 40
+                50 10
+                30 60
                 
                 """;
 
         String expected = """
-                
+                3
                 """;
         Stream.of(input.split("\\n")).map(String::trim).forEach(in::inputln);
         A42.main(null);
