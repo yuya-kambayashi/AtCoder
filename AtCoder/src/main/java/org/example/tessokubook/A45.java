ac32c81972497c420d52afc8458d5f562776a864
yuya-kambayashi <kambayashi73@gmail.com>
2025-05-28 22:23:40 +0900

    tessokubook

--- AtCoder/src/main/java/org/example/tessokubook/A45.java
+++ AtCoder/src/main/java/org/example/tessokubook/A45.java
@@ -9,6 +9,7 @@ import org.example.template.StandardInputSnatcher;
 import org.example.template.StandardOutputSnatcher;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
@@ -36,27 +37,46 @@ public class A45 {
         Scanner sc = new Scanner(System.in);
 
         final int n = sc.nextInt();
+        final String b = sc.next();
+        String s = sc.next();
 
-        System.out.println(n);
+        int score = 0;
+        for (int i = 0; i < n; i++) {
+            char c = s.charAt(i);
+            if (c == 'R') {
+                score += 2;
+            } else if (c == 'B') {
+                score += 1;
+            }
+        }
+        score %= 3;
+        if (score == 2 && b.equals("R") || score == 1 && b.equals("B") || score == 0 && b.equals("W")) {
+            System.out.println("Yes");
+        } else {
+            System.out.println("No");
+        }
     }
+
 //}
 
-    //@Test
+    @Test
     public void Case1() {
 
         String input = """
+                4 B
+                WBBR
                 
                 """;
 
         String expected = """
-                
+                Yes
                 """;
         Stream.of(input.split("\\n")).map(String::trim).forEach(in::inputln);
         A45.main(null);
         Stream.of(expected.split("\\n")).map(String::trim).forEach(s -> assertThat(out.readLine().trim()).isEqualTo(s));
     }
 
-    //@Test
+    @Test
     public void Case2() {
 
         String input = """
