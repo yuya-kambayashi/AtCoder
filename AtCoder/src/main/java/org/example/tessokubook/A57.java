f1f645be5ff6e8dc9a2417ec253cb3a2c681c9a0
yuya-kambayashi <kambayashi73@gmail.com>
2025-05-31 20:55:04 +0900

    tessokubook

--- AtCoder/src/main/java/org/example/tessokubook/A57.java
+++ AtCoder/src/main/java/org/example/tessokubook/A57.java
@@ -32,10 +32,30 @@ public class A57 {
 //import java.util.*;
 //public class Main {
 
+    // https://atcoder.jp/contests/tessoku-book/submissions/51145784
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         final int n = sc.nextInt();
+        final int q = sc.nextInt();
+        int[] aa = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            aa[i] = sc.nextInt();
+        }
+        int[][] dp = new int[30][n + 1];
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                dp[i][j] = dp[i - 1][dp[i - 1][j]];
+            }
+        }
+        for (int i = 0; i < q; i++) {
+            int x = sc.nextInt() - 1;
+            int y = sc.nextInt() - 1;
+
+
+        }
+
 
         System.out.println(n);
     }
@@ -45,10 +65,20 @@ public class A57 {
     public void Case1() {
 
         String input = """
+                7 4
+                2 4 1 7 6 5 3
+                1 1
+                1 5
+                2 13
+                5 999999999
                 
                 """;
 
         String expected = """
+                2
+                1
+                3
+                6
                 
                 """;
         Stream.of(input.split("\\n")).map(String::trim).forEach(in::inputln);
