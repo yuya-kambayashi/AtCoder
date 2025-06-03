e3425395fbfc940172bd51d38a51cf59159000a2
yuya-kambayashi <kambayashi73@gmail.com>
2025-06-03 23:01:51 +0900

    tessokubook

--- AtCoder/src/main/java/org/example/tessokubook/A58.java
+++ AtCoder/src/main/java/org/example/tessokubook/A58.java
@@ -4,6 +4,7 @@ import java.util.*;
 import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.InstanceOfAssertFactories.set;
 
 import org.example.template.StandardInputSnatcher;
 import org.example.template.StandardOutputSnatcher;
@@ -33,24 +34,52 @@ public class A58 {
 //import java.util.*;
 //public class Main {
 
+    // https://atcoder.jp/contests/tessoku-book/submissions/56629236
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         final int n = sc.nextInt();
         final int q = sc.nextInt();
 
-        for (int i = 0; i < n; i++) {
+        final var size = Integer.highestOneBit(n) << 1;
+        final var segTree = new int[size * 2];
+
+        for (int i = 0; i < q; i++) {
             int t = sc.nextInt();
+
             if (t == 1) {
-                int p = sc.nextInt();
-                int x = sc.nextInt();
+
+                int pos = sc.nextInt();
+                int value = sc.nextInt();
+
+                int index = pos + size - 1;
+                segTree[index] = value;
+                index /= 2;
+                while (index > 0) {
+                    segTree[index] = Math.max(segTree[index * 2], segTree[index * 2 + 1]);
+                    index /= 2;
+                }
             } else if (t == 2) {
                 int l = sc.nextInt();
                 int r = sc.nextInt();
+                System.out.println(query(segTree, l, r, 1, size + 1, 1));
+
             }
         }
+    }
 
-        System.out.println(n);
+    static int query(int[] segTree, int l, int r, int a, int b, int u) {
+        if (r <= a || b <= l) {
+            return Integer.MIN_VALUE;
+        }
+        if (l <= a && b <= r) {
+            return segTree[u];
+        }
+        int mid = (a + b) / 2;
+        int left = query(segTree, l, r, a, mid, u * 2);
+        int right = query(segTree, l, r, mid, b, u * 2 + 1);
+        return Math.max(left, right);
     }
 //}
 
