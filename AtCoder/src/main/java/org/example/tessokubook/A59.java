8b01e29dc9c7d40871d3cd2c22fa14f85e9c93fb
yuya-kambayashi <kambayashi73@gmail.com>
2025-06-04 23:16:49 +0900

    tessokubook

--- AtCoder/src/main/java/org/example/tessokubook/A59.java
+++ AtCoder/src/main/java/org/example/tessokubook/A59.java
@@ -4,11 +4,13 @@ import java.util.*;
 import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.InstanceOfAssertFactories.set;
 
 import org.example.template.StandardInputSnatcher;
 import org.example.template.StandardOutputSnatcher;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
@@ -36,19 +38,62 @@ public class A59 {
         Scanner sc = new Scanner(System.in);
 
         final int n = sc.nextInt();
+        final int q = sc.nextInt();
 
-        System.out.println(n);
+        int size = Integer.highestOneBit(n) << 1;
+        int[] segTree = new int[size * 2];
+
+        for (int i = 0; i < q; i++) {
+            int type = sc.nextInt();
+            if (type == 1) {
+                int pos = sc.nextInt();
+                int value = sc.nextInt();
+                int index = pos + size - 1;
+                segTree[index] = value;
+                index /= 2;
+                while (index > 0) {
+                    segTree[index] = segTree[index * 2] + segTree[index * 2 + 1];
+                    index /= 2;
+                }
+
+            } else if (type == 2) {
+                int l = sc.nextInt();
+                int r = sc.nextInt();
+                System.out.println(query(segTree, l, r, 1, size + 1, 1));
+            }
+
+        }
+    }
+
+    static int query(int[] segTree, int l, int r, int a, int b, int u) {
+        if (r <= a || b <= l) {
+            return 0;
+        }
+        if (l <= a && b <= r) {
+            return segTree[u];
+        }
+        int mid = (a + b) / 2;
+        int left = query(segTree, l, r, a, mid, u * 2);
+        int right = query(segTree, l, r, mid, b, u * 2 + 1);
+        return left + right;
     }
 //}
 
-    //@Test
+    @Test
     public void Case1() {
 
         String input = """
+                8 4
+                1 3 16
+                1 6 24
+                2 4 8
+                2 1 7
                 
                 """;
 
         String expected = """
+                24
+                40
                 
                 """;
         Stream.of(input.split("\\n")).map(String::trim).forEach(in::inputln);
