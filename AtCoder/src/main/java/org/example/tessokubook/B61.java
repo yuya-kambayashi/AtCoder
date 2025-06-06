22285c4561931377c94b25859ebef777276ed858
yuya-kambayashi <kambayashi73@gmail.com>
2025-06-06 23:31:45 +0900

    tessokubook

--- AtCoder/src/main/java/org/example/tessokubook/B61.java
+++ AtCoder/src/main/java/org/example/tessokubook/B61.java
@@ -9,6 +9,7 @@ import org.example.template.StandardInputSnatcher;
 import org.example.template.StandardOutputSnatcher;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
@@ -36,34 +37,88 @@ public class B61 {
         Scanner sc = new Scanner(System.in);
 
         final int n = sc.nextInt();
+        final int m = sc.nextInt();
 
-        System.out.println(n);
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < m; i++) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            map.put(a, map.getOrDefault(a, 0) + 1);
+            map.put(b, map.getOrDefault(b, 0) + 1);
+        }
+        int max = Integer.MIN_VALUE;
+        int maxKey = Integer.MIN_VALUE;
+        for (var entry : map.entrySet()) {
+            if (max < entry.getValue()) {
+                max = entry.getValue();
+                maxKey = entry.getKey();
+            }
+        }
+
+        System.out.println(maxKey);
     }
 //}
 
-    //@Test
+    @Test
     public void Case1() {
 
         String input = """
+                5 4
+                1 2
+                2 3
+                3 4
+                3 5
                 
                 """;
 
         String expected = """
-                
+                3
                 """;
         Stream.of(input.split("\\n")).map(String::trim).forEach(in::inputln);
-        B61.main(null);
+        A61.main(null);
         Stream.of(expected.split("\\n")).map(String::trim).forEach(s -> assertThat(out.readLine().trim()).isEqualTo(s));
     }
 
-    //@Test
+    @Test
     public void Case2() {
 
         String input = """
+                15 30
+                6 9
+                9 10
+                2 9
+                9 12
+                2 14
+                1 4
+                4 6
+                1 3
+                4 14
+                1 6
+                9 11
+                2 6
+                3 9
+                5 9
+                4 9
+                11 15
+                1 13
+                4 13
+                8 9
+                9 13
+                5 15
+                3 5
+                8 10
+                2 4
+                9 14
+                1 9
+                2 8
+                6 13
+                7 9
+                9 15
                 
                 """;
 
         String expected = """
+                9
                 
                 """;
         Stream.of(input.split("\\n")).map(String::trim).forEach(in::inputln);
