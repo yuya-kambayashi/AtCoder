5a2a59f0995dd461a41ef108f39ff1f4e06d632a
yuya-kambayashi <kambayashi73@gmail.com>
2025-06-09 23:20:04 +0900

    tessokubook

--- AtCoder/src/main/java/org/example/tessokubook/A62.java
+++ AtCoder/src/main/java/org/example/tessokubook/A62.java
@@ -9,6 +9,7 @@ import org.example.template.StandardInputSnatcher;
 import org.example.template.StandardOutputSnatcher;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
@@ -36,19 +37,61 @@ public class A62 {
         Scanner sc = new Scanner(System.in);
 
         final int n = sc.nextInt();
+        final int m = sc.nextInt();
 
-        System.out.println(n);
+        UnionFind uf = new UnionFind(n);
+        for (int i = 0; i < m; i++) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            uf.unite(a, b);
+        }
+        int root = uf.find(1);
+        for (int i = 2; i <= n; i++) {
+            if (uf.find(i) != root) {
+                System.out.println("The graph is not connected.");
+                return;
+            }
+        }
+
+        System.out.println("The graph is connected.");
+    }
+
+    static class UnionFind {
+        int[] parent;
+
+        UnionFind(int n) {
+            parent = new int[n + 1];
+            for (int i = 1; i <= n; i++) parent[i] = i;
+        }
+
+        int find(int x) {
+            if (parent[x] != x) parent[x] = find(parent[x]);
+            return parent[x];
+        }
+
+        void unite(int x, int y) {
+            parent[find(x)] = find(y);
+        }
+
+        boolean same(int x, int y) {
+            return find(x) == find(y);
+        }
     }
 //}
 
-    //@Test
+    @Test
     public void Case1() {
 
         String input = """
+                3 2
+                1 3
+                2 3
                 
                 """;
 
         String expected = """
+                The graph is connected.
+                
                 
                 """;
         Stream.of(input.split("\\n")).map(String::trim).forEach(in::inputln);
@@ -56,14 +99,22 @@ public class A62 {
         Stream.of(expected.split("\\n")).map(String::trim).forEach(s -> assertThat(out.readLine().trim()).isEqualTo(s));
     }
 
-    //@Test
+    @Test
     public void Case2() {
 
         String input = """
+                6 6
+                1 4
+                2 3
+                3 4
+                5 6
+                1 2
+                2 4
                 
                 """;
 
         String expected = """
+                The graph is not connected.
                 
                 """;
         Stream.of(input.split("\\n")).map(String::trim).forEach(in::inputln);
