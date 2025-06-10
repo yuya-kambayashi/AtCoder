e30a33c33152fd7014a65074d904fcc37ca996f3
yuya-kambayashi <kambayashi73@gmail.com>
2025-06-10 15:36:17 +0900

    tessokubook

--- AtCoder/src/main/java/org/example/tessokubook/A65.java
+++ AtCoder/src/main/java/org/example/tessokubook/A65.java
@@ -9,6 +9,7 @@ import org.example.template.StandardInputSnatcher;
 import org.example.template.StandardOutputSnatcher;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
@@ -36,19 +37,32 @@ public class A65 {
         Scanner sc = new Scanner(System.in);
 
         final int n = sc.nextInt();
+        int[] aa = new int[n + 1];
+        for (int i = 2; i <= n; i++) {
+            aa[i] = sc.nextInt();
+        }
+        int[] dp = new int[n + 1];
+        for (int i = n; i > 1; i--) {
+            dp[aa[i]] += dp[i] + 1;
+        }
+        for (int i = 1; i <= n; i++) {
+            System.out.print(dp[i] + " ");
+        }
 
-        System.out.println(n);
     }
 //}
 
-    //@Test
+    @Test
     public void Case1() {
 
         String input = """
+                7
+                1 1 3 2 4 4
                 
                 """;
 
         String expected = """
+                6 1 3 2 0 0 0
                 
                 """;
         Stream.of(input.split("\\n")).map(String::trim).forEach(in::inputln);
@@ -56,14 +70,17 @@ public class A65 {
         Stream.of(expected.split("\\n")).map(String::trim).forEach(s -> assertThat(out.readLine().trim()).isEqualTo(s));
     }
 
-    //@Test
+    @Test
     public void Case2() {
 
         String input = """
+                15
+                1 2 1 1 1 6 2 6 9 10 6 12 13 12
                 
                 """;
 
         String expected = """
+                14 2 0 0 0 8 0 0 2 1 0 3 1 0 0
                 
                 """;
         Stream.of(input.split("\\n")).map(String::trim).forEach(in::inputln);
