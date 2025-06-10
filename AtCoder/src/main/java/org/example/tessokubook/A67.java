92388bb4ce8f15b9d8cc0f193d8cb38de4e0add1
yuya-kambayashi <kambayashi73@gmail.com>
2025-06-10 21:33:26 +0900

    tessokubook

--- AtCoder/src/main/java/org/example/tessokubook/A67.java
+++ AtCoder/src/main/java/org/example/tessokubook/A67.java
@@ -9,6 +9,7 @@ import org.example.template.StandardInputSnatcher;
 import org.example.template.StandardOutputSnatcher;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
@@ -28,28 +29,85 @@ public class A67 {
         System.setIn(null);
         System.setOut(null);
     }
-//import java.math.*;
+
+    //import java.math.*;
 //import java.util.*;
 //public class Main {
+    record E(int from, int to, int cost) {
+    }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         final int n = sc.nextInt();
+        final int m = sc.nextInt();
+        E[] ee = new E[m];
+        for (int i = 0; i < m; i++) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            int c = sc.nextInt();
 
-        System.out.println(n);
+            ee[i] = new E(a, b, c);
+        }
+        Arrays.sort(ee, (e1, e2) -> e1.cost - e2.cost);
+
+        UnionFind uf = new UnionFind(n);
+
+        long ans = 0;
+        for (int i = 0; i < m; i++) {
+            int from = uf.find(ee[i].from);
+            int to = uf.find(ee[i].to);
+            if (from != to) {
+                uf.unite(from, to);
+                ans += ee[i].cost;
+            }
+        }
+
+        System.out.println(ans);
+    }
+
+    static class UnionFind {
+        int[] parent;
+
+        UnionFind(int n) {
+            parent = new int[n + 1];
+            for (int i = 1; i <= n; i++) parent[i] = i;
+        }
+
+        int find(int x) {
+            if (parent[x] != x) parent[x] = find(parent[x]);
+            return parent[x];
+        }
+
+        void unite(int x, int y) {
+            parent[find(x)] = find(y);
+        }
+
+        boolean same(int x, int y) {
+            return find(x) == find(y);
+        }
     }
 //}
 
-    //@Test
+    @Test
     public void Case1() {
 
         String input = """
+                7 9
+                1 2 12
+                1 3 10
+                2 6 160
+                2 7 15
+                3 4 1
+                3 5 4
+                4 5 3
+                4 6 120
+                6 7 14
                 
                 """;
 
         String expected = """
-                
+                55
                 """;
         Stream.of(input.split("\\n")).map(String::trim).forEach(in::inputln);
         A67.main(null);
