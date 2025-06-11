c46f25b9419f8e8d26defc77f619b08201da25f6
yuya-kambayashi <kambayashi73@gmail.com>
2025-06-11 23:46:27 +0900

    tessokubook

--- AtCoder/src/main/java/org/example/tessokubook/A69.java
+++ AtCoder/src/main/java/org/example/tessokubook/A69.java
@@ -4,11 +4,13 @@ import java.util.*;
 import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.InstanceOfAssertFactories.set;
 
 import org.example.template.StandardInputSnatcher;
 import org.example.template.StandardOutputSnatcher;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
@@ -32,24 +34,59 @@ public class A69 {
 //import java.util.*;
 //public class Main {
 
+    // https://atcoder.jp/contests/tessoku-book/submissions/59803439
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         final int n = sc.nextInt();
+        char[][] ccc = new char[n][n];
+        for (int i = 0; i < n; i++) {
+            ccc[i] = sc.next().toCharArray();
+        }
+        int[] match = new int[n];
+        Arrays.fill(match, -1);
 
-        System.out.println(n);
+        int ret = 0;
+        for (int student = 0; student < n; student++) {
+            boolean[] visited = new boolean[n];
+            if (canMath(student, visited, match, ccc, n)) {
+                ret++;
+            }
+        }
+
+        System.out.println(ret);
+    }
+
+    static boolean canMath(int student, boolean[] visited, int[] match, char[][] ccc, int n) {
+        for (int seat = 0; seat < n; seat++) {
+            if (ccc[seat][student] == '#' && !visited[seat]) {
+                visited[seat] = true;
+                if (match[seat] == -1 || canMath(match[seat], visited, match, ccc, n)) {
+                    match[seat] = student;
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 //}
 
-    //@Test
+    @Test
     public void Case1() {
 
         String input = """
+                5
+                #....
+                #.#..
+                ....#
+                ....#
+                ...##
                 
                 """;
 
         String expected = """
-                
+                4
                 """;
         Stream.of(input.split("\\n")).map(String::trim).forEach(in::inputln);
         A69.main(null);
