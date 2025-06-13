2c28e40222d1ac5a527ca1abf625503c741fef40
yuya-kambayashi <kambayashi73@gmail.com>
2025-06-13 23:03:05 +0900

    tessokubook

--- AtCoder/src/main/java/org/example/tessokubook/A70.java
+++ AtCoder/src/main/java/org/example/tessokubook/A70.java
@@ -9,6 +9,7 @@ import org.example.template.StandardInputSnatcher;
 import org.example.template.StandardOutputSnatcher;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
@@ -28,28 +29,86 @@ public class A70 {
         System.setIn(null);
         System.setOut(null);
     }
-//import java.math.*;
+
+    //import java.math.*;
 //import java.util.*;
 //public class Main {
+
+    // https://atcoder.jp/contests/tessoku-book/submissions/66431997
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        final int n = sc.nextInt();
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        boolean[] visited = new boolean[1 << n];
+        int start = 0;
 
-        System.out.println(n);
+        for (int i = 0; i < n; i++) {
+            int a = sc.nextInt();
+            if (a == 1) {
+                start += (1 << i);
+            }
+        }
+        List<Operation> opes = new ArrayList<>();
+        for (int i = 0; i < m; i++) {
+            opes.add(new Operation(sc.nextInt() - 1, sc.nextInt() - 1, sc.nextInt() - 1));
+        }
+
+        Deque<int[]> que = new ArrayDeque<>();
+        que.offer(new int[]{start, 0});
+
+        while (!que.isEmpty()) {
+            int[] cur = que.poll();
+            if (visited[cur[0]]) {
+                continue;
+            }
+            visited[cur[0]] = true;
+
+            if (cur[0] == (1 << n) - 1) {
+                System.out.println(cur[1]);
+                return;
+            }
+
+            for (int i = 0; i < m; i++) {
+                int next = cur[0];
+                next ^= (1 << opes.get(i).x);
+                next ^= (1 << opes.get(i).y);
+                next ^= (1 << opes.get(i).z);
+
+                que.offer(new int[]{next, cur[1] + 1});
+            }
+
+        }
+
+
+        System.out.println(-1);
+    }
+
+    static class Operation {
+        int x, y, z;
+
+        Operation(int x, int y, int z) {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
     }
 //}
 
-    //@Test
+    @Test
     public void Case1() {
 
         String input = """
+                4 2
+                0 1 1 0
+                1 2 3
+                2 3 4
                 
                 """;
 
         String expected = """
-                
+                2
                 """;
         Stream.of(input.split("\\n")).map(String::trim).forEach(in::inputln);
         A70.main(null);
