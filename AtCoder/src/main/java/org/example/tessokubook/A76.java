6fb964488a71055bdced9dc9320c9554054c841c
yuya-kambayashi <kambayashi73@gmail.com>
2025-06-16 14:09:47 +0900

    tessokubook

--- AtCoder/src/main/java/org/example/tessokubook/A76.java
+++ AtCoder/src/main/java/org/example/tessokubook/A76.java
@@ -9,6 +9,7 @@ import org.example.template.StandardInputSnatcher;
 import org.example.template.StandardOutputSnatcher;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
@@ -34,22 +35,55 @@ public class A76 {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
+        int mod = 1000000007;
         final int n = sc.nextInt();
+        final int w = sc.nextInt();
+        final int l = sc.nextInt();
+        final int r = sc.nextInt();
+        int[] xx = new int[n + 2];
+        for (int i = 1; i <= n; i++) {
+            xx[i] = sc.nextInt();
+        }
+        xx[n + 1] = w;
 
-        System.out.println(n);
+        int[] dp = new int[n + 2];
+        int[] sum = new int[n + 2];
+        dp[0] = 1;
+        sum[0] = 1;
+        for (int i = 1; i <= n + 1; i++) {
+            int posl = Arrays.binarySearch(xx, xx[i] - r);
+            int posr = Arrays.binarySearch(xx, xx[i] - l + 1);
+            posl = posl >= 0 ? posl : ~posl;
+            posr = posr >= 0 ? posr : ~posr;
+            posr--;
+
+            if (posr >= 0) {
+                dp[i] += sum[posr];
+            }
+            if (posl >= 1) {
+                dp[i] -= sum[posl - 1];
+            }
+            dp[i] = (dp[i] + mod) % mod;
+
+            sum[i] = sum[i - 1] + dp[i];
+            sum[i] %= mod;
+        }
+
+        System.out.println(dp[n + 1]);
     }
 //}
 
-    //@Test
+    @Test
     public void Case1() {
 
         String input = """
+                5 65 7 37
+                5 15 30 50 55
                 
                 """;
 
         String expected = """
-                
+                7
                 """;
         Stream.of(input.split("\\n")).map(String::trim).forEach(in::inputln);
         A76.main(null);
