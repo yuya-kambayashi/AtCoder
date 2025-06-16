3a9050b144b96286e7eb15f0d220cc174837bab1
yuya-kambayashi <kambayashi73@gmail.com>
2025-06-16 11:07:08 +0900

    ABC410

--- AtCoder/src/main/java/org/example/tessokubook/A75.java
+++ AtCoder/src/main/java/org/example/tessokubook/A75.java
@@ -9,6 +9,7 @@ import org.example.template.StandardInputSnatcher;
 import org.example.template.StandardOutputSnatcher;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
@@ -36,20 +37,43 @@ public class A75 {
         Scanner sc = new Scanner(System.in);
 
         final int n = sc.nextInt();
+        int[][] td = new int[n][2];
+        for (int i = 0; i < n; i++) {
+            td[i][0] = sc.nextInt();
+            td[i][1] = sc.nextInt();
+        }
+        Arrays.sort(td, (td1, td2) -> td1[1] - td2[1]);
 
-        System.out.println(n);
+        int[][] dp = new int[n + 1][1441];
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= 1440; j++) {
+                dp[i][j] = Math.max(dp[i][j], dp[i - 1][j]);
+                dp[i][j] = Math.max(dp[i][j], dp[i][j - 1]);
+
+                if (j >= td[i - 1][0] && j <= td[i - 1][1]) {
+                    dp[i][j] = Math.max(dp[i][j], dp[i - 1][j - td[i - 1][0]] + 1);
+                }
+            }
+        }
+        System.out.println(dp[n][1440]);
     }
 //}
 
-    //@Test
+    @Test
     public void Case1() {
 
         String input = """
+                4
+                20 70
+                30 50
+                30 100
+                20 60
                 
                 """;
 
         String expected = """
-                
+                4
                 """;
         Stream.of(input.split("\\n")).map(String::trim).forEach(in::inputln);
         A75.main(null);
