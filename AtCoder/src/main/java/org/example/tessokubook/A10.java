911157f3be2321d9cab54dfb4c188e6057ac6f2d
yuya-kambayashi <kambayashi73@gmail.com>
2025-06-18 15:19:57 +0900

    tessokubook

--- AtCoder/src/main/java/org/example/tessokubook/A10.java
+++ AtCoder/src/main/java/org/example/tessokubook/A10.java
@@ -37,19 +37,43 @@ public class A10 {
         Scanner sc = new Scanner(System.in);
 
         final int n = sc.nextInt();
+        int[][] aa = new int[3][n + 2];
+        for (int i = 1; i <= n; i++) {
+            aa[0][i] = sc.nextInt();
+        }
+        for (int i = 1; i <= n; i++) {
+            aa[1][i] = Math.max(aa[1][i - 1], aa[0][i]);
+        }
+        for (int i = n; i >= 1; i--) {
+            aa[2][i] = Math.max(aa[2][i + 1], aa[0][i]);
+        }
 
-        System.out.println(n);
+        int d = sc.nextInt();
+        for (int i = 0; i < d; i++) {
+            int ans = 0;
+            int l = sc.nextInt();
+            int r = sc.nextInt();
+            System.out.println(Math.max(aa[1][l - 1], aa[2][r + 1]));
+        }
+
     }
 //}
 
-    //@Test
+    @Test
     public void Case1() {
 
         String input = """
+                7
+                1 2 5 5 2 3 1
+                2
+                3 5
+                4 6
                 
                 """;
 
         String expected = """
+                3
+                5
                 
                 """;
         Stream.of(input.split("\\n")).map(String::trim).forEach(in::inputln);
