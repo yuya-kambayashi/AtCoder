31441317eb00ac7b41ee3b4bcaa6887e7abb300f
yuya-kambayashi <kambayashi73@gmail.com>
2025-07-01 17:57:10 +0900

    tessokubook

--- AtCoder/src/main/java/org/example/ABC/ABC412D.java
+++ AtCoder/src/main/java/org/example/ABC/ABC412D.java
@@ -37,17 +37,43 @@ public class ABC412D {
         Scanner sc = new Scanner(System.in);
 
         final int n = sc.nextInt();
-        int ans = 0;
-        for (int i = 0; i < n; i++) {
-            int a = sc.nextInt();
-            int b = sc.nextInt();
+        final int m = sc.nextInt();
+        boolean[][] ggg = new boolean[n][n];
+        for (int i = 0; i < m; i++) {
+            int a = sc.nextInt() - 1;
+            int b = sc.nextInt() - 1;
+            ggg[a][b] = true;
+            ggg[b][a] = true;
+        }
 
-            if (a < b) {
-                ans++;
+        for (int bit = 0; bit < (1 << n); bit++) {
+            for (int i = 0; i < n; i++) {
+                if ((bit & (1 << i)) != 0) {
+                    continue;
+                }
+                boolean[][] ttt = new boolean[n][n];
+                for (int j = 0; j < n; j++) {
+                    ttt = Arrays.copyOf(ggg, n);
+                }
+                if (i > 0){
+                    ggg[i]
+                }
             }
         }
 
+
         System.out.println(ans);
+    }
+
+    static boolean chack(boolean[][] ggg) {
+        boolean ok = true;
+        for (int i = 0; i < ggg.length; i++) {
+            if (ggg[i].length != 2) {
+                ok = false;
+                break;
+            }
+        }
+        return ok;
     }
 //}
 
@@ -55,13 +81,17 @@ public class ABC412D {
     public void Case1() {
 
         String input = """
-                AtCoder
-                Total
+                5 4
+                1 2
+                1 5
+                2 4
+                4 5
+                
                 
                 """;
 
         String expected = """
-                Yes
+                3
                 
                 """;
         Stream.of(input.split("\\n")).map(String::trim).forEach(in::inputln);
@@ -73,13 +103,12 @@ public class ABC412D {
     public void Case2() {
 
         String input = """
-                aBCdE
-                abcdcba
+                3 0
                 
                 """;
 
         String expected = """
-                No
+                3
                 """;
         Stream.of(input.split("\\n")).map(String::trim).forEach(in::inputln);
         ABC412D.main(null);
@@ -90,28 +119,57 @@ public class ABC412D {
     public void Case3() {
 
         String input = """
-                abcde
-                XYZ
+                6 8
+                1 4
+                1 5
+                2 3
+                2 6
+                3 4
+                3 6
+                4 5
+                4 6
                 
                 """;
 
         String expected = """
-                Yes
+                2
                 """;
         Stream.of(input.split("\\n")).map(String::trim).forEach(in::inputln);
         ABC412D.main(null);
         Stream.of(expected.split("\\n")).map(String::trim).forEach(s -> assertThat(out.readLine().trim()).isEqualTo(s));
     }
 
-    //@Test
+    @Test
     public void Case4() {
 
         String input = """
+                8 21
+                1 4
+                5 7
+                8 4
+                3 4
+                2 5
+                8 1
+                5 1
+                2 8
+                2 1
+                2 4
+                3 1
+                6 7
+                5 8
+                2 7
+                6 8
+                5 4
+                3 8
+                7 3
+                7 8
+                5 3
+                7 4
                 
                 """;
 
         String expected = """
-                
+                13
                 """;
         Stream.of(input.split("\\n")).map(String::trim).forEach(in::inputln);
         ABC412D.main(null);
