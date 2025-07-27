7edd96f97452943d7b374a8da3db78742ece72fe
yuya-kambayashi <kambayashi73@gmail.com>
2025-07-27 19:45:33 +0900

    tessokubook

---
+++ AtCoder/src/main/java/org/example/ABC/ABC416D.java
@@ -0,0 +1,103 @@
+package org.example.ABC;
+
+import java.util.*;
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.example.template.StandardInputSnatcher;
+import org.example.template.StandardOutputSnatcher;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.TestInstance;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class ABC416D {
+
+    private final StandardInputSnatcher in = new StandardInputSnatcher();
+    private final StandardOutputSnatcher out = new StandardOutputSnatcher();
+
+    @BeforeAll
+    public void beforeAll() {
+        System.setIn(in);
+        System.setOut(out);
+    }
+
+    @AfterAll
+    public void afterAll() {
+        System.setIn(null);
+        System.setOut(null);
+    }
+//import java.math.*;
+//import java.util.*;
+//public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        final int n = sc.nextInt();
+
+        System.out.println(n);
+    }
+//}
+
+    //@Test
+    public void Case1() {
+
+        String input = """
+                
+                """;
+
+        String expected = """
+                
+                """;
+        Stream.of(input.split("\\n")).map(String::trim).forEach(in::inputln);
+        ABC416D.main(null);
+        Stream.of(expected.split("\\n")).map(String::trim).forEach(s -> assertThat(out.readLine().trim()).isEqualTo(s));
+    }
+
+    //@Test
+    public void Case2() {
+
+        String input = """
+                
+                """;
+
+        String expected = """
+                
+                """;
+        Stream.of(input.split("\\n")).map(String::trim).forEach(in::inputln);
+        ABC416D.main(null);
+        Stream.of(expected.split("\\n")).map(String::trim).forEach(s -> assertThat(out.readLine().trim()).isEqualTo(s));
+    }
+
+    //@Test
+    public void Case3() {
+
+        String input = """
+                
+                """;
+
+        String expected = """
+                
+                """;
+        Stream.of(input.split("\\n")).map(String::trim).forEach(in::inputln);
+        ABC416D.main(null);
+        Stream.of(expected.split("\\n")).map(String::trim).forEach(s -> assertThat(out.readLine().trim()).isEqualTo(s));
+    }
+
+    //@Test
+    public void Case4() {
+
+        String input = """
+                
+                """;
+
+        String expected = """
+                
+                """;
+        Stream.of(input.split("\\n")).map(String::trim).forEach(in::inputln);
+        ABC416D.main(null);
+        Stream.of(expected.split("\\n")).map(String::trim).forEach(s -> assertThat(out.readLine().trim()).isEqualTo(s));
+    }
+}
